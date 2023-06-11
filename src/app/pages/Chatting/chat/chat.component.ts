83a70e60c0054a6bdb0f8f015f211b02017ba3e8
natha <wagner57280@gmail.com>
2023-06-11 19:54:39 +0200

    scss chat

--- src/app/pages/Chatting/chat/chat.component.ts
+++ src/app/pages/Chatting/chat/chat.component.ts
@@ -2,6 +2,7 @@ import { Component, OnInit } from '@angular/core';
 import {Channel} from "../../../model/Channel";
 import {ChannelService} from "../../../service/channel.service";
 import {ActivatedRoute} from "@angular/router";
+import {WebSocketService} from "../../../service/web-socket.service";
 @Component({
   selector: 'app-chat',
   templateUrl: './chat.component.html',
@@ -10,13 +11,23 @@ import {ActivatedRoute} from "@angular/router";
 export class ChatComponent implements OnInit {
   channel: Channel | undefined;
 
-  constructor(private channelService: ChannelService,private route: ActivatedRoute) { }
+  constructor(private channelService: ChannelService,private route: ActivatedRoute,private socketService: WebSocketService) { }
+
+
+  public messages: string[] = [];
+  public newMessage: string = '';
+
+
 
   ngOnInit(): void {
     this.route.params.subscribe(params => {
       const channelId = params['id']; // ou toute autre clé que vous utilisez pour l'identifiant du canal
       if (channelId) {
         this.getChannel(channelId);
+        this.socketService.connect();
+        this.socketService.messages$.subscribe((message) => {
+          this.messages.push(message);
+        });
       }
   });
   }
@@ -31,5 +42,10 @@ export class ChatComponent implements OnInit {
           console.error(error);
         }
       );
+  }
+
+  public sendMessage(): void {
+    this.socketService.sendMessage(this.newMessage);
+    this.newMessage = '';
   }
 }
